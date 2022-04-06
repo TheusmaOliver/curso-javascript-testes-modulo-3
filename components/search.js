fe43b91343ed62a6f77febf0ba91bd8f084e9248
Matheus Rodrigues <81190214+TheusmaOliver@users.noreply.github.com>
2022-04-06 15:37:57 -0300

    create input text test

--- components/search.js
+++ components/search.js
@@ -1,6 +1,13 @@
+import { useState } from 'react';
+
 export default function Search({ doSearch }) {
+  const [searchTerm, setSearchTerm] = useState('');
   return (
-    <form onSubmit={doSearch} name="search-form" className="relative mt-6 max-w-lg mx-auto">
+    <form
+      onSubmit={() => doSearch(searchTerm)}
+      name="search-form"
+      className="relative mt-6 max-w-lg mx-auto"
+    >
       <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
         <svg className="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
           <path
@@ -14,8 +21,10 @@ export default function Search({ doSearch }) {
       </span>
 
       <input
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
         className="w-full border rounded-md pl-10 pr-4 py-2 focus:border-blue-500 focus:outline-none focus:shadow-outline"
-        type="text"
+        type="search"
         placeholder="Search"
       />
     </form>
--- components/search.spec.js
+++ components/search.spec.js
@@ -1,5 +1,6 @@
 import Search from './search';
 import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 const doSearch = jest.fn();
 
 describe('Search', () => {
@@ -7,6 +8,12 @@ describe('Search', () => {
     render(<Search doSearch={doSearch} />);
 
     expect(screen.getByRole('form')).toBeInTheDocument();
+  });
+
+  it('should render a search input', () => {
+    render(<Search doSearch={doSearch} />);
+
+    expect(screen.getByRole('searchbox')).toHaveProperty('type', 'search');
   });
 
   it('should call doSearch() when form is submitted', async () => {
@@ -17,5 +24,18 @@ describe('Search', () => {
     await fireEvent.submit(form);
 
     expect(doSearch).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call doSearch() with the value of the input', async () => {
+    render(<Search doSearch={doSearch} />);
+
+    const inputText = 'some text here';
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('searchbox');
+
+    await userEvent.type(input, inputText);
+    await fireEvent.submit(form);
+
+    expect(doSearch).toHaveBeenCalledWith(inputText);
   });
 });
